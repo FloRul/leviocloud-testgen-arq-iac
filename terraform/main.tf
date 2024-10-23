aa1a0153001727aaf1348a4ef0f86bc5d1cf9379
FloRul <rumiel.florian@gmail.com>
2024-10-23 09:26:17 -0400

    firs commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.72.1"
+    }
+
+  }
+}
+
+provider "aws" {
+  profile = "arq-iac"
+  region  = "us-east-1"
+  default_tags {
+    tags = {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Project     = "leviocloud-testgen-arq-iac"
+    }
+  }
+}
