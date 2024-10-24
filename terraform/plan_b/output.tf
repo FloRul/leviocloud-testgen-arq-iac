afe95b9b595f6961bbd18fce295033442dd00d2a
FloRul <rumiel.florian@gmail.com>
2024-10-24 09:58:16 -0400

    plan B

---
+++ terraform/plan_b/output.tf
@@ -0,0 +1,26 @@
+﻿
+# Outputs
+output "lambda_function_name" {
+  description = "Name of the Lambda function"
+  value       = module.lambda_function.lambda_function_name
+}
+
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = module.lambda_function.lambda_function_arn
+}
+
+output "input_bucket_name" {
+  description = "Name of the input S3 bucket"
+  value       = module.s3_bucket_input.s3_bucket_id
+}
+
+output "output_bucket_name" {
+  description = "Name of the output S3 bucket"
+  value       = module.s3_bucket_output.s3_bucket_id
+}
+
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic"
+  value       = module.sns_topic.sns_topic_arn
+}
