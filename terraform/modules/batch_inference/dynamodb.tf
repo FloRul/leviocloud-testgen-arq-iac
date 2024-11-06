b2f713fbb5340bb05aa6fb0547db5a40572c7482
FloRul <rumiel.florian@gmail.com>
2024-11-06 09:09:59 -0500

    swap range and hash key

--- terraform/modules/batch_inference/dynamodb.tf
+++ terraform/modules/batch_inference/dynamodb.tf
@@ -1,8 +1,8 @@
 ﻿resource "aws_dynamodb_table" "inference_jobs_status_table" {
   name         = "${var.project_name}-inference-jobs-status-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "job_id"
-  range_key    = "user_id"
+  hash_key     = "user_id"
+  range_key    = "job_id"
 
   attribute {
     name = "job_id"
