a3f8e63cad4ee080d12cb7bcef85c0dbed8cb9e5
FloRul <rumiel.florian@gmail.com>
2024-11-06 13:22:38 -0500

    remove output

--- terraform/modules/hosting/output.tf
+++ terraform/modules/hosting/output.tf
@@ -1,7 +1,8 @@
 ﻿output "s3_bucket" {
   value = {
     name = module.s3.s3_bucket_id
-  arn = module.s3.s3_bucket_arn }
+    arn  = module.s3.s3_bucket_arn
+  }
 }
 
 output "s3_bucket_regional_domain_name" {
